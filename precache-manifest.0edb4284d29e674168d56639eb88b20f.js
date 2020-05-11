00e55b8c5d659c49f9cc79c0221cd9aaeb76df95
thecerealcoder <thecerealcoder@gmail.com>
2020-05-10 20:49:27 -0500

    Updates

---
+++ precache-manifest.0edb4284d29e674168d56639eb88b20f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758a0143017ff318fcd35b71c812db45",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "0cdd1f30c8a34c40cafe",
+    "url": "/covid-19-tracker/static/css/main.48f32bee.chunk.css"
+  },
+  {
+    "revision": "10c4f51779e85bb2bc83",
+    "url": "/covid-19-tracker/static/js/2.f1de93f1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.f1de93f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdd1f30c8a34c40cafe",
+    "url": "/covid-19-tracker/static/js/main.66e923c1.chunk.js"
+  },
+  {
+    "revision": "16c98dd34293354e45ed",
+    "url": "/covid-19-tracker/static/js/runtime-main.56b37832.js"
+  }
+]);
